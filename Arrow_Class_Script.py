e51928e3191ec0b8265b24167fda5037df28338f
djbrock98 <47343406+djbrock98@users.noreply.github.com>
2019-03-04 20:31:32 -0600

    Add files via upload

---
+++ Arrow_Class_Script.py
@@ -0,0 +1,17 @@
+# Derrek Brock
+# Created 2-26-19
+# Last Edited 2-26-19
+
+python -m pip install numpy
+# Installs numpy library
+
+class Arrow:
+    # Creates Arrow class and related information
+    def__init__(self, position, velocity, aircraft_priority):
+        self.position = [0, 0, 0]
+        # Sets position of aircraft
+        self.velocity = [0, 0, 0]
+        # Sets velocity of aircraft
+        self.aircraft_priority = 3
+        # Sets aircraft priority for the aircraft 
+        # High priority number = high priority, low priority number = low priority
